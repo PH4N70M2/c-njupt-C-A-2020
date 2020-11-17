b43fe626886a4a195b10a45cd30a4bdc82f18a09
deadkiwi14 <74499007+deadkiwi14@users.noreply.github.com>
2020-11-17 10:47:28 +0800

    Create 1.c

---
+++ Chapter2/1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+
+	int a=sizeof(char);
+	int b=sizeof(int);
+	int c=sizeof(short);
+	int d=sizeof(long);
+	int e=sizeof(unsigned int);
+	int f=sizeof(float);
+	int g=sizeof(double);
+	printf("sizeof(char)=%d\n",a);
+	
+	printf("sizeof(int)=%d\n",b);
+	
+	printf("sizeof(short)=%d\n",c);
+	
+	printf("sizeof(long)=%d\n",d);
+	
+	printf("sizeof(unsigned int)=%d\n",e);
+	
+	printf("sizeof(float)=%d\n",f);
+	
+	printf("sizeof(double)=%d\n",g);
+
+	return 0;
+}
