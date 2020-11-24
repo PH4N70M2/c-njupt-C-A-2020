168a73d34f6c3233d0574f5bdd071f8f09a5aca3
deadkiwi14 <74499007+deadkiwi14@users.noreply.github.com>
2020-11-24 11:23:52 +0800

    Create 7-4-4-advanced.c

---
+++ Chapter7/7-4-4-advanced.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int joseph(int n, int m){
+	int x=0,i=2;
+	for(;i<=n;i++)
+		x=(x+m)%i;
+	return x;
+}
+
+int josephRE(int n, int m){
+	return (n==1)?0:(josephRE(n-1,m)+m)%n;
+}
+
+int main(){
+	int n,m;
+	scanf("%d%d",&n,&m);
+	printf("%d",josephRE(n,m)+1); //it starts from 0. To satisfy the OJ requirement, I add 1 to it.
+	return 0;
+}
