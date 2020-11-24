ac26a7853dd8b2918dd281e0d918a32095ba1c7a
deadkiwi14 <74499007+deadkiwi14@users.noreply.github.com>
2020-11-24 15:37:24 +0800

    Update 2-4-1.c

--- Chapter2/2-4-1.c
+++ Chapter2/2-4-1.c
@@ -8,18 +8,13 @@ int main(){
 	int e=sizeof(unsigned int);
 	int f=sizeof(float);
 	int g=sizeof(double);
+	
 	printf("sizeof(char)=%d\n",a);
-	
 	printf("sizeof(int)=%d\n",b);
-	
 	printf("sizeof(short)=%d\n",c);
-	
 	printf("sizeof(long)=%d\n",d);
-	
 	printf("sizeof(unsigned int)=%d\n",e);
-	
 	printf("sizeof(float)=%d\n",f);
-	
 	printf("sizeof(double)=%d\n",g);
 
 	return 0;
